9609154856a180a66edac1c674d7228cf95ced66
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-08-29 00:41:32 +0300

    update base entity

--- Core/Entities/Concrete/BaseFullEntity.cs
+++ Core/Entities/Concrete/BaseFullEntity.cs
@@ -5,6 +5,6 @@ namespace Core.Entities.Concrete
     public abstract class BaseFullEntity : BaseStateEntity
     {
         public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
-        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual DateTime? ModifiedDate { get; set; }
     }
 }
