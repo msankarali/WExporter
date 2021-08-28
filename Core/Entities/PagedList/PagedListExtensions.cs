11756986736201706ad297e6e36c617011dd6df9
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-08-29 00:59:49 +0300

    add paging

---
+++ Core/Entities/PagedList/PagedListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Core.Entities.PagedList
+{
+    public static class PagedListExtensions
+    {
+        public static IPagedList<T> ToPagedList<T>(this IQueryable<T> source, int pageSize, int pageNumber = 0)
+        {
+            int count = source.Count();
+            int totalPages = (int)Math.Ceiling((double)count / pageSize);
+            if (totalPages == 0) totalPages = 1;
+            if (pageNumber >= totalPages) throw new Exception("404 ^__^");
+            var pagedList = source.Skip(pageNumber * pageSize).Take(pageSize);
+            return new PagedList<T>(pageNumber, pageSize, count, totalPages, pagedList.ToList());
+        }
+    }
+}
