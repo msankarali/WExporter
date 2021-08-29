c3be0631be4b29d6e164940b45d838d7eba24a72
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-08-29 23:57:41 +0300

    add business module

---
+++ WExporter.Business/DependencyResolvers/AutofacModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Castle.DynamicProxy;
+using Core.Interceptors;
+using Module = Autofac.Module;
+
+namespace WExporter.Business.DependencyResolvers
+{
+    class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly()).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new InterceptorSelector()
+                }).SingleInstance().InstancePerDependency();
+        }
+    }
+}
