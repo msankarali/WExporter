12afc2e56b7c58f2d5bd718ea90cb8923e18b395
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-08-29 14:38:41 +0300

    add result pattern

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public ResultType ResultType { get; }
+
+        public List<string> Messages { get; private set; }
+
+        public int Code { get; set; }
+
+        public Result(ResultType resultType)
+        {
+            ResultType = resultType;
+        }
+
+        public IResult AddMessage(string message)
+        {
+            if (Messages == null) Messages = new List<string>();
+            Messages.Add(message);
+            return this;
+        }
+    }
+}
