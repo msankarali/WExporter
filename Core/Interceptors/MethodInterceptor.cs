d7fd30d5147fce90fca0cdabd4a801d5c3c3e31e
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-08-29 17:30:30 +0300

    add interceptor base classes

---
+++ Core/Interceptors/MethodInterceptor.cs
@@ -0,0 +1,42 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interceptors
+{
+    public abstract class MethodInterceptor : MethodInterceptorAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+
+        protected virtual void OnAfter(IInvocation invocation) { }
+
+        protected virtual void OnException(IInvocation invocation, Exception e) { }
+
+        protected virtual void OnSuccess(IInvocation invocation) { }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            bool isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation, e);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+            OnAfter(invocation);
+        }
+    }
+}
---
+++ Core/Interceptors/MethodInterceptorAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace Core.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public abstract class MethodInterceptorAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}
