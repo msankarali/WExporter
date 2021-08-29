402190d334a3540302763b07c004bde99303949f
Muharrem Servet Ankarali <mservetankarali@gmail.com>
2021-08-29 14:55:33 +0300

    add business runner

---
+++ Core/Utilities/Business/BusinessRunner.cs
@@ -0,0 +1,33 @@
+﻿using Core.Utilities.Results;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRunner
+    {
+        public static IResult CheckAnyDifferent(ResultType resultType, params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (resultType != logic.ResultType)
+                {
+                    return logic;
+                }
+            }
+
+            return null;
+        }
+
+        public static IResult CheckAny(ResultType resultType, params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (resultType == logic.ResultType)
+                {
+                    return logic;
+                }
+            }
+
+            return null;
+        }
+    }
+}
